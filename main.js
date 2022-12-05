4313f23435c38ebdc84966fc21021858bcf8a4eb
dontroll17 <dontroll@yandex.ru>
2022-12-05 20:03:42 +0300

    refactor: change title and passed the class

--- main.js
+++ main.js
@@ -1,31 +1,6 @@
 import * as THREE from 'three';
 import { GUI } from 'dat.gui';
-
-class AxisGridHelper {
-  constructor(node, units = 10) {
-    const axes = new THREE.AxesHelper();
-    axes.material.depthTest = false;
-    axes.renderOrder = 2; 
-    node.add(axes);
- 
-    const grid = new THREE.GridHelper(units, units);
-    grid.material.depthTest = false;
-    grid.renderOrder = 1;
-    node.add(grid);
- 
-    this.grid = grid;
-    this.axes = axes;
-    this.visible = false;
-  }
-  get visible() {
-    return this._visible;
-  }
-  set visible(v) {
-    this._visible = v;
-    this.grid.visible = v;
-    this.axes.visible = v;
-  }
-}
+import AxesGridHelper  from './AxisGridHelper.js';
 
 function main() {
   const canvas = document.querySelector('#canvas');
@@ -89,7 +64,7 @@ function main() {
   objects.push(moonMesh);
 
   const makeAxisGrid = (node, label, units) => {
-    const helper = new AxisGridHelper(node, units);
+    const helper = new AxesGridHelper(node, units);
     gui.add(helper, 'visible').name(label);
   }
 
